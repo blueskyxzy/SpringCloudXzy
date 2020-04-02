9f37eab8f8a4f96c259cd88e25f70e1534541a7e
blueskyxzy <blueskyxzy@gmail.com>
2020-04-02 16:17:59 +0800

    framework change

--- nacosTest-interface/src/main/java/com/xzy/service/DemoService.java
+++
@@ -1,7 +0,0 @@
-package com.xzy.service;
-
-public interface DemoService {
-
-    String sayHello(String name);
-
-}
---
+++ nacosTest-interface/src/main/java/com/xzy/service/UserService.java
@@ -0,0 +1,11 @@
+package com.xzy.service;
+
+/**
+ * Created by xzy on 2020/4/2  .
+ */
+
+public interface UserService {
+
+    String getUser(Long userId);
+
+}
